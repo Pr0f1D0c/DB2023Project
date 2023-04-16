410a977959ef8d538c9468117cc618c78ba281bc
= <starokozhko.ig>
2023-04-17 02:47:46 +0300

    Add amateur and transactions into schema(DDL)

--- scripts/tabels_creation_DDL.sql
+++ scripts/tabels_creation_DDL.sql
@@ -52,3 +52,30 @@ CREATE TABLE IF NOT EXISTS variation (
     pgn VARCHAR(255) NOT NULL,
     PRIMARY KEY (ECO, variation)
 );
+
+CREATE TABLE IF NOT EXISTS amateur_game (
+    id INT PRIMARY KEY,
+    white INT NOT NULL,
+    black INT NOT NULL,
+    result VARCHAR(7) DEFAULT '1/2-1/2',
+    pgn VARCHAR(1023),
+    opening VARCHAR(3),
+    variation VARCHAR(255),
+    dt DATE
+);
+
+CREATE TABLE IF NOT EXISTS amateur (
+    id INT NOT NULL PRIMARY KEY,
+    nickname TEXT,
+    country VARCHAR(3),
+    last_transaction INTEGER NOT NULL,
+    login varchar(63)
+);
+
+CREATE TABLE IF NOT EXISTS premium_transaction (
+    id INT NOT NULL PRIMARY KEY,
+    player INT NOT NULL,
+    dt DATE NOT NULL,
+    premium_duration INTERVAL NOT NULL,
+    card_number VARCHAR(16) NOT NULL
+)
