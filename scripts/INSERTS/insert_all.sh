8ecfbd7f7fc729be55f236001cfc9932ab69ab5f
= <starokozhko.ig>
2023-04-23 23:15:20 +0300

    edit min insert_all script

--- scripts/INSERTS/insert_all.sh
+++ scripts/INSERTS/insert_all.sh
@@ -2,6 +2,8 @@
 psql -h localhost -p 5432 -U postgres -d project --password <<OMG
 begin;
 \i ../tabels_creation_DDL.sql
+\i ../triggers/premium_insert_trigger.sql
+\i ../triggers/update_player_rating_trigger.sql
 
 \copy chess.player from '../../data/csv/player.csv' DELIMITER ',' CSV HEADER;
 \copy chess.opening from '../../data/csv/opening.csv' DELIMITER ',' CSV HEADER;
@@ -12,6 +14,12 @@ begin;
 \i ./openings_insert.sql
 \i ./tournament_insert.sql
 \i ./game_insert.sql
+\i ./update_player_history.sql
+
+\i ./amateur_insert.sql
+\i ./premium_transaction_insert.sql
+\i ./amateur_game_insert.sql
+
 commit;
 OMG
 
